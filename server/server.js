e4c2287ce0fa8ae0fe716dcbbe6a1d3e9d24915a
YafitSamuel <gyafit642@gmail.com>
2022-03-06 10:13:08 +0200

    commit

--- server/server.js
+++ server/server.js
@@ -4,22 +4,24 @@ require('./DB/db');
 const express=require('express')
 const app = express()
 const cors=require('cors') 
-
+const passport = require("passport");
+require("./config/passport")(passport);
 const AnimalRouter=require('./routes/animals.route')
 const employeeRouter=require('./routes/employee-route')
-
 app.use(cors())
 app.use(express.json())
-app.use('/animals',AnimalRouter)
-app.use('/employee',employeeRouter)
+app.use(passport.initialize()); //Initialize the passport function on the server
+app.use('/animals',passport.authenticate("jwt", { session: false }),AnimalRouter)
+app.use("/employee", passport.authenticate("jwt", { session: false }), employeeRouter);
+// app.use('/employee',employeeRouter)
 app.get('/',(req,res)=>{res.send('connection up ')})
 
 app.listen(process.env.PORT )
 
-const passport = require("passport");
-require("./config/passport")(passport);
-app.use(passport.initialize()); //Initialize the passport function on the server
-app.use("/employee", passport.authenticate("jwt", { session: false }), employeeRouter);
+
+
+
+
 
 
 
